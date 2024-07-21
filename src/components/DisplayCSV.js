8c15e0eb75ad666b1c1dcfc2d4a7a9b1e972dc3c
P-nasi <pnasimonan@gmail.com>
2024-07-21 14:41:38 +0900

    見た目変更、None対応

--- src/components/DisplayCSV.js
+++ src/components/DisplayCSV.js
@@ -39,16 +39,32 @@ const DisplayCSV = ({ csvData, handleEdit, handleDelete }) => {
                             <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">{row['サークル名']}</h5>
-                                    <p className="card-text">{row['大学名']}</p>
-                                    <p className="card-text">{row['都道府県']}</p>
-                                    <p className="card-text">{row['活動内容']}</p>
-                                    <p className="card-text">{row['設置区分']}</p>
-                                    <p className="card-text">{row['X']}</p>
-                                    <p className="card-text">{row['HomePage']}</p>
-                                    <p className="card-text">{row['学校コード']}</p>
-                                    <p className="card-text">{row['設立年度']}</p>
+                                    {row['大学名'] && row['大学名'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">大学名：</span>{row['大学名']}</p>
+                                    )}
+                                    {row['都道府県'] && row['都道府県'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">都道府県：</span>{row['都道府県']}</p>
+                                    )}
+                                    {row['活動内容'] && row['活動内容'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">活動内容：</span>{row['活動内容']}</p>
+                                    )}
+                                    {row['設置区分'] && row['設置区分'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">設置区分：</span>{row['設置区分']}</p>
+                                    )}
+                                    {row['X'] && row['X'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">X：</span>{row['X']}</p>
+                                    )}
+                                    {row['HomePage'] && row['HomePage'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">HomePage：</span>{row['HomePage']}</p>
+                                    )}
+                                    {row['学校コード'] && row['学校コード'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">学校コード：</span>{row['学校コード']}</p>
+                                    )}
+                                    {row['設立年度'] && row['設立年度'] !== 'None' && (
+                                        <p className="card-text"><span className="card-text-label">設立年度：</span>{row['設立年度']}</p>
+                                    )}
                                     <h6 className='card-title2'>参加大会</h6>
-                                    <ul>
+                                    <ul className='invite_university'>
                                         {['インカレ', '学ロボ', 'キャチロボ', 'ロボット相撲大会', 'ETロボコン', '知能ロボコン', '水中ロボコン', '海洋ロボコン', 'ひこロボ', '鳥人間コンテスト', '種コン', 'モデロケ全国大会', '能代宇宙イベント', 'ARLISS', 'ALL JAコンテスト', '学生フォーミュラ', 'エコラン', 'U-22 プログラミング', 'ICPC', 'SECCON', 'ISUCON', 'iGEM'].filter(competition => row[competition] === true || row[competition] === "True").map((competition) => (
                                             <li key={competition}>{competition}</li>
                                         ))}
